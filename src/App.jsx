cb25fc55a361ccc5e301ad45373a19e09a327978
sebasaczuk <seba.saczuk@gmail.com>
2023-06-15 11:38:43 -0300

    Final con Ajustes y CapturaGIF

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,35 @@
 import React from "react";
+import { Link, BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/navbar/Navbar";
 import ItemListContainer from "../src/components/pages/productList/ItemListContainer";
 import ProductDetails from "../src/components/pages/productDetails/ProductDetails";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Container, Typography, AppBar, Toolbar, Button } from "@mui/material";
 
 const App = () => {
   return (
-    <Container>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <a
-              href="http://localhost:5173"
-              style={{
-                textDecoration: "none",
-                color: "inherit",
-              }}
-            >
-              Pharmastock
-            </a>
-          </Typography>
-          <Navbar />
-        </Toolbar>
-      </AppBar>
-      <Typography variant="div" sx={{ mt: 2 }}>
-        <br />
-        <Router>
+    <Router>
+      {" "}
+      {/* Meto todos los componentes con el enrutador */}
+      <Container>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+                Pharmastock
+              </Link>
+            </Typography>
+            <Navbar />
+          </Toolbar>
+        </AppBar>
+        <Typography variant="div" sx={{ mt: 2 }}>
+          <br />
           <Routes>
-            <Route exact path="/" element={<ItemListContainer />} />
-            <Route exact path="/product/:id" element={<ProductDetails />} />
+            <Route path="/" element={<ItemListContainer />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
           </Routes>
-        </Router>
-      </Typography>
-    </Container>
+        </Typography>
+      </Container>
+    </Router>
   );
 };
 
